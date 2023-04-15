96513ad75f0d31cc484e3dfb52b7ac273c75b706
skydreamer21 <skydreamer210@gmail.com>
2023-04-15 15:24:16 +0900

    chore: 레포지토리 setup

---
+++ src/main/java/SampleClass.java
@@ -0,0 +1,15 @@
+public class SampleClass {
+    int cnt;
+    
+    public SampleClass(int cnt) {
+        this.cnt = cnt;
+    }
+    
+    public void increase() {
+        cnt++;
+    }
+    
+    public int getCnt() {
+        return cnt;
+    }
+}
