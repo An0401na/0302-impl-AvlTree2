f9c11038400c50cdadc8f178212172091b6d47ef
An Na <99172832+An0401na@users.noreply.github.com>
2023-04-18 03:04:50 +0900

    fix: Node 클래스 수정

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,4 +1,14 @@
 public class Node {
     int value, height;
+    Node parent;
     Node left, right;
+
+    public Node(int value) {
+        this.value = value;
+        this.height = 1;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+    }
+
 }
